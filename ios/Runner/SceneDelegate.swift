a722e61f8a3012795026cc480853257b73ee63d3
ssc2991lyh <qw3458801249@163.com>
2026-03-01 04:40:40 +0800

    fix: downgrade ios swift code for build compatibility

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -1,6 +1,10 @@
 import Flutter
 import UIKit
 
-class SceneDelegate: FlutterSceneDelegate {
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
 
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UISceneConnectionOptions) {
+        // 使用默认的 Flutter 场景处理，如果不需要多窗口可以保持为空
+    }
 }
